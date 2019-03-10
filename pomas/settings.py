9f613c86927b431b69f22590fa7aa22f2d9e2b2a
jajajasalu2 <8iamthewalrus8@gmail.com>
2019-03-10 12:08:12 +0530

    reports update and delete works

--- pomas/settings.py
+++ pomas/settings.py
@@ -129,6 +129,8 @@ USE_L10N = True
 USE_TZ = True
 
 AUTH_USER_MODEL='profiles.MyUser'
+
+# APPEND_SLASH=False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
--- reports/views.py
+++ reports/views.py
@@ -6,7 +6,8 @@ from datetime import datetime
 #import pdfkit
 
 # Create your views here.
-def transcript_html(request,group_id):
+#def transcript_html(request,group_id):
+def reports(request,group_id):
     current_user = request.user
     current_user_groups = request.user.groups.all()
     group = Group.objects.get(id=group_id)
@@ -15,7 +16,7 @@ def transcript_html(request,group_id):
         context = {
             'reports': reports
         }
-        return render(request,'reports/transcript.html',context)
+        return render(request,'reports/reports.html',context)
     else:
         return redirect('home')
 
@@ -44,6 +45,34 @@ def create(request):
         print('user not teacher')
         return redirect('home')
 
+
+def delete(request):
+    current_user = request.user
+    current_user_groups = request.user.groups.all()
+    if (current_user.is_teacher):
+        report = Report.objects.get(id=request.POST['id'])
+        group = report.group_id
+        report.delete()
+        return redirect('reports/reports/'+str(group.id))
+    else:
+        return redirect('home')
+       
+
+def update(request):
+    current_user = request.user
+    current_user_groups = request.user.groups.all()
+    if (current_user.is_teacher):
+        report_id = request.POST['id']
+        report = Report.objects.get(id=report_id)
+        group = report.group_id
+        remark = request.POST['remark']
+        rating = request.POST['rating']
+        report = Report(id=report_id,group_id_id=group,remark=remark,rating=rating)
+        report.save()
+        print('report saved')
+        return redirect('/reports/reports/'+str(group.id))
+    else:
+        return redirect('home')
  
 
 #def transcript_pdf(request,group_id):
