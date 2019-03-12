5c1d94a9ea2a96ca99981fded17c879ac39a7083
Raj <you@example.com>
2019-03-12 11:28:31 +0530

    fixed requirements

--- pomas/settings.py
+++ pomas/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'pomas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pomas',
-        'USER': 'phpadmin',
-        'PASSWORD': '8UxYzQ!o',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'pomas',
+#        'USER': 'phpadmin',
+#        'PASSWORD': '8UxYzQ!o',
+#        'HOST': 'localhost',
+#        'PORT': ''
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
